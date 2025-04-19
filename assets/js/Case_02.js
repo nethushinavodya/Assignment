e05f48e5b4dba86acb9ac2401ac723fb9a17efe1
nethushinavodya <nethushiperera@gmail.com>
2025-04-19 14:22:04 +0530

    Assignment-css

---
+++ assets/js/Case_02.js
@@ -0,0 +1,12 @@
+let colours = ["red", "green", "blue", "yellow", "mediumpurple", "deeppink"];
+
+setInterval(() => {
+    let first = colours.pop();
+    colours.unshift(first);
+    document.getElementById("first").style.backgroundColor = colours[0];
+    document.getElementById("second").style.backgroundColor = colours[1];
+    document.getElementById("third").style.backgroundColor = colours[2];
+    document.getElementById("fourth").style.backgroundColor = colours[3];
+    document.getElementById("fifth").style.backgroundColor = colours[4];
+    document.getElementById("last").style.backgroundColor = colours[5];
+}, 2000);
